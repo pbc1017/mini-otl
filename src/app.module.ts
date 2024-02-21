f6c7a1e7c761e3601f521beca3b483136b622f26
pbc1017 <knsol2@naver.com>
2024-02-21 20:04:33 +0900

    make skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, DepartmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
